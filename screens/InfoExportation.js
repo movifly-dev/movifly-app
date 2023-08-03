dd14460371f858da9acb12c566773ad4149a8252
MarcusRosaa <marcusvrosa1@gmail.com>
2023-08-03 03:15:22 -0300

    feat: create export function

--- screens/InfoExportation.js
+++ screens/InfoExportation.js
@@ -1,24 +1,165 @@
-import { SafeAreaView, ScrollView, Text, View } from 'react-native';
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, Text, View, TouchableOpacity, StyleSheet, Modal } from 'react-native';
+import { useMain } from '../contexts/MainContext';
+import * as FileSystem from 'expo-file-system';
+import * as Sharing from 'expo-sharing'; // Import expo-sharing
+
+function ModalMessage({ isVisible, message, onClose, onSettingsPress }) {
+  return (
+    <Modal animationType="fade" transparent visible={isVisible}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalMessageText}>{message}</Text>
+          <TouchableOpacity onPress={onClose} style={[styles.modalButton, styles.okButton]}>
+            <Text style={styles.buttonText}>OK</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={onSettingsPress} style={[styles.modalButton, styles.settingsButton]}>
+            <Text style={styles.buttonText}>Settings</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+}
 
 function InfoExportationView() {
-  // ================================================================
+  const { clients } = useMain();
+  const [isWritePermissionGranted, setIsWritePermissionGranted] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  useEffect(() => {
+    const checkPermissions = async () => {
+      const { status } = await FileSystem.getPermissionsAsync();
+      setIsWritePermissionGranted(status === 'granted');
+    };
+
+    checkPermissions();
+  }, []);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const openAppSettings = async () => {
+    // ... (No need for app settings related to Media Library)
+  };
+
+  const exportToCSV = async () => {
+    if (!isWritePermissionGranted) {
+      const { status } = await FileSystem.requestPermissionsAsync();
+      setIsWritePermissionGranted(status === 'granted');
+      if (status !== 'granted') {
+        showModal();
+        return;
+      }
+    }
+    if (clients.length === 0) {
+      return; // No clients to export
+    }
+
+    const csvHeader = 'Id,DataVenda,CompanhiaAerea,Localizador,NomePassageiro,NomeComprador,NomeVendedor,ContatoVendedor,ValorCompra,ValorVenda,Lucro,FormaPagamento,ChecklistPago,EmailCliente,CPF\n';
+    const csvData = clients.map((client) => `${client.id},${client.dataVenda},${client.companhiaAerea},${client.localizador},${client.nomePassageiro},${client.nomeComprador},${client.nomeVendedor},${client.contatoVendedor},${client.valorCompra},${client.valorVenda},${client.lucro},${client.formaPagamento},${client.checklistPago},${client.emailCliente},${client.cpf}\n`).join('');
+
+    const csvContent = csvHeader + csvData;
+
+    try {
+      const fileUri = FileSystem.documentDirectory + 'clients.csv';
+
+      await FileSystem.writeAsStringAsync(fileUri, csvContent, {
+        encoding: FileSystem.EncodingType.UTF8,
+      });
+
+      console.log('CSV file created:', fileUri);
+
+      // Share the file using expo-sharing
+      const sharingAvailable = await Sharing.isAvailableAsync();
+      if (sharingAvailable) {
+        await Sharing.shareAsync(fileUri);
+      } else {
+        throw new Error('Error criando/baixando arquivo');
+      }
+
+    } catch (error) {
+      throw new Error('Erro ao criar/baixar arquivo: ' + error);
+    }
+  };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.infoExportationView}>
-
+          <TouchableOpacity style={styles.exportButton} onPress={exportToCSV}>
+            <Text style={styles.exportButtonText}>Exportar Clientes</Text>
+          </TouchableOpacity>
         </View>
+        <ModalMessage
+          isVisible={isModalVisible}
+          message="Write permission not granted. Please grant the permission in the app settings to export the CSV file."
+          onClose={closeModal}
+          onSettingsPress={openAppSettings}
+        />
       </ScrollView>
     </SafeAreaView>
   );
 }
 
-export default InfoExportationView;
-
-const styles = {
+const styles = StyleSheet.create({
   infoExportationView: {
     flex: 1,
     padding: 20,
+    alignItems: 'center',
   },
-};
+  exportButton: {
+    backgroundColor: '#007BFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    borderRadius: 5,
+    paddingHorizontal: 20,
+  },
+  exportButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalMessageText: {
+    fontSize: 18,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  modalButton: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  okButton: {
+    backgroundColor: '#007BFF',
+  },
+  settingsButton: {
+    backgroundColor: 'gray',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default InfoExportationView;
--- screens/ProfitCalculator.js
+++ screens/ProfitCalculator.js
@@ -1,22 +1,161 @@
-import { SafeAreaView, ScrollView, Text, View } from "react-native";
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, Text, View, TextInput, TouchableOpacity, StyleSheet, Modal } from 'react-native';
 
 function ProfitCalculatorView() {
-  // ================================================================
+  const [valorCompra, setValorCompra] = useState('');
+  const [valorVenda, setValorVenda] = useState('');
+  const [descontosTaxas, setDescontosTaxas] = useState('');
+  const [profit, setProfit] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const calculateProfit = () => {
+    if (!valorCompra || !valorVenda) {
+      setModalVisible(true);
+      return;
+    }
+
+    const compra = parseFloat(valorCompra);
+    const venda = parseFloat(valorVenda);
+    const descontos = descontosTaxas ? parseFloat(descontosTaxas) : 0;
+
+    const calculatedProfit = venda - compra - descontos;
+    setProfit(calculatedProfit);
+  };
 
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.profitCalculatorView}></View>
+        <View style={styles.profitCalculatorView}>
+          <Text style={styles.label}>Valor da Compra:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(value) => setValorCompra(value)}
+            value={valorCompra}
+            keyboardType="numeric"
+            placeholder="Digite o valor da compra"
+          />
+
+          <Text style={styles.label}>Valor da Venda:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(value) => setValorVenda(value)}
+            value={valorVenda}
+            keyboardType="numeric"
+            placeholder="Digite o valor da venda"
+          />
+
+          <Text style={styles.label}>Descontos/Taxas:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(value) => setDescontosTaxas(value)}
+            value={descontosTaxas}
+            keyboardType="numeric"
+            placeholder="Digite o valor dos descontos/taxas (opcional)"
+          />
+
+          <TouchableOpacity style={styles.calculateButton} onPress={calculateProfit}>
+            <Text style={styles.calculateButtonText}>Calcular</Text>
+          </TouchableOpacity>
+
+          {profit !== null && (
+            <View style={styles.profitResult}>
+              <Text style={styles.profitResultLabel}>Lucro:</Text>
+              <Text style={styles.profitResultValue}>{profit >= 0 ? profit.toFixed(2) : '0.00'}</Text>
+
+              <Text style={styles.profitResultLabel}>Prejuízo:</Text>
+              <Text style={styles.profitResultValue}>{profit < 0 ? (profit * -1).toFixed(2) : '0.00'}</Text>
+            </View>
+          )}
+
+          <Modal animationType="fade" transparent={true} visible={modalVisible}>
+            <View style={styles.modalContainer}>
+              <View style={styles.modalContent}>
+                <Text style={styles.modalMessage}>Por favor, insira o Valor da Compra e o Valor da Venda.</Text>
+                <TouchableOpacity style={styles.modalButton} onPress={() => setModalVisible(false)}>
+                  <Text style={styles.modalButtonText}>OK</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
-export default ProfitCalculatorView;
-
-const styles = {
+const styles = StyleSheet.create({
   profitCalculatorView: {
     flex: 1,
     padding: 20,
   },
-};
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  calculateButton: {
+    backgroundColor: '#007BFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  calculateButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  profitResult: {
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#ccc',
+  },
+  profitResultLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+  profitResultValue: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 5,
+  },
+  modalMessage: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  modalButton: {
+    backgroundColor: '#007BFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40,
+    borderRadius: 5,
+  },
+  modalButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default ProfitCalculatorView;
