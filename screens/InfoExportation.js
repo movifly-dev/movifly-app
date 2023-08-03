2784d600aded2f01df84de0f01ca0995a8dc70f0
MarcusRosaa <marcusvrosa1@gmail.com>
2023-08-03 04:19:24 -0300

    feat: create export permission and open in excel xlsx file

--- screens/InfoExportation.js
+++ screens/InfoExportation.js
@@ -1,115 +1,83 @@
-/* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, Text, View, TouchableOpacity, StyleSheet, Modal } from 'react-native';
+import React from 'react';
+import { SafeAreaView, ScrollView, Text, View, TouchableOpacity, StyleSheet, Platform, Linking } from 'react-native';
 import { useMain } from '../contexts/MainContext';
+import XLSX from 'xlsx';
 import * as FileSystem from 'expo-file-system';
-import * as Sharing from 'expo-sharing'; // Import expo-sharing
+import * as Sharing from 'expo-sharing';
 
-function ModalMessage({ isVisible, message, onClose, onSettingsPress }) {
-  return (
-    <Modal animationType="fade" transparent visible={isVisible}>
-      <View style={styles.modalContainer}>
-        <View style={styles.modalContent}>
-          <Text style={styles.modalMessageText}>{message}</Text>
-          <TouchableOpacity onPress={onClose} style={[styles.modalButton, styles.okButton]}>
-            <Text style={styles.buttonText}>OK</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={onSettingsPress} style={[styles.modalButton, styles.settingsButton]}>
-            <Text style={styles.buttonText}>Settings</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </Modal>
-  );
-}
-
-function InfoExportationView() {
+function ExcelExportView() {
   const { clients } = useMain();
-  const [isWritePermissionGranted, setIsWritePermissionGranted] = useState(false);
-  const [isModalVisible, setIsModalVisible] = useState(false);
 
-  useEffect(() => {
-    const checkPermissions = async () => {
-      const { status } = await FileSystem.getPermissionsAsync();
-      setIsWritePermissionGranted(status === 'granted');
-    };
-
-    checkPermissions();
-  }, []);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const closeModal = () => {
-    setIsModalVisible(false);
-  };
-
-  const openAppSettings = async () => {
-    // ... (No need for app settings related to Media Library)
-  };
-
-  const exportToCSV = async () => {
-    if (!isWritePermissionGranted) {
-      const { status } = await FileSystem.requestPermissionsAsync();
-      setIsWritePermissionGranted(status === 'granted');
-      if (status !== 'granted') {
-        showModal();
-        return;
-      }
-    }
+  const exportToExcel = async () => {
     if (clients.length === 0) {
-      return; // No clients to export
+      // No clients to export
+      return;
     }
 
-    const csvHeader = 'Id,DataVenda,CompanhiaAerea,Localizador,NomePassageiro,NomeComprador,NomeVendedor,ContatoVendedor,ValorCompra,ValorVenda,Lucro,FormaPagamento,ChecklistPago,EmailCliente,CPF\n';
-    const csvData = clients.map((client) => `${client.id},${client.dataVenda},${client.companhiaAerea},${client.localizador},${client.nomePassageiro},${client.nomeComprador},${client.nomeVendedor},${client.contatoVendedor},${client.valorCompra},${client.valorVenda},${client.lucro},${client.formaPagamento},${client.checklistPago},${client.emailCliente},${client.cpf}\n`).join('');
+    const csvHeader = [
+      'DataVenda', 'CompanhiaAerea', 'Localizador', 'NomePassageiro', 'NomeComprador', 'NomeVendedor', 'ContatoVendedor', 'ValorCompra', 'ValorVenda', 'Lucro', 'FormaPagamento', 'ChecklistPago', 'EmailCliente', 'CPF'
+    ];
 
-    const csvContent = csvHeader + csvData;
+    const csvData = clients.map(client => [
+      client.dataVenda, client.companhiaAerea, client.localizador, client.nomePassageiro,
+      client.nomeComprador, client.nomeVendedor, client.contatoVendedor, client.valorCompra, client.valorVenda, client.lucro, client.formaPagamento, client.checklistPago, client.emailCliente, client.cpf
+    ]);
+    const data = [csvHeader, ...csvData];
+    const ws = XLSX.utils.aoa_to_sheet(data);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Clients');
+
+    // Generate a unique file name for the Excel file
+    const fileName = `clients_${Date.now()}.xlsx`;
+    const fileUri = FileSystem.documentDirectory + fileName;
+
+    const wbout = XLSX.write(wb, { type: 'base64', bookType: 'xlsx' });
 
     try {
-      const fileUri = FileSystem.documentDirectory + 'clients.csv';
+      await FileSystem.writeAsStringAsync(fileUri, wbout, { encoding: FileSystem.EncodingType.Base64 });
+      console.log('Excel file created:', fileUri);
 
-      await FileSystem.writeAsStringAsync(fileUri, csvContent, {
-        encoding: FileSystem.EncodingType.UTF8,
-      });
+      // Now you have the Excel file at 'fileUri', you can use it as needed.
+      // For example, you can share the file using the 'expo-sharing' package or trigger a download.
 
-      console.log('CSV file created:', fileUri);
-
-      // Share the file using expo-sharing
-      const sharingAvailable = await Sharing.isAvailableAsync();
-      if (sharingAvailable) {
-        await Sharing.shareAsync(fileUri);
-      } else {
-        throw new Error('Error criando/baixando arquivo');
+      if (Platform.OS === 'android') {
+        // Trigger a download on Android
+        const downloadUrl = fileUri;
+        Sharing.shareAsync(downloadUrl);
+      } else if (Platform.OS === 'ios') {
+        // Open the file in Safari on iOS
+        const fileURL = `file://${fileUri}`;
+        Linking.openURL(fileURL)
+          .then(() => {
+            console.log('File opened successfully in Safari.');
+          })
+          .catch((error) => {
+            console.log('Error opening file:', error);
+          });
       }
-
     } catch (error) {
-      throw new Error('Erro ao criar/baixar arquivo: ' + error);
+      throw new Error('Error creating Excel file:' + error);
     }
   };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View style={styles.infoExportationView}>
-          <TouchableOpacity style={styles.exportButton} onPress={exportToCSV}>
-            <Text style={styles.exportButtonText}>Exportar Clientes</Text>
+        <View style={styles.exportView}>
+          <TouchableOpacity
+            style={styles.exportButton}
+            onPress={exportToExcel}
+          >
+            <Text style={styles.exportButtonText}>Export to Excel</Text>
           </TouchableOpacity>
         </View>
-        <ModalMessage
-          isVisible={isModalVisible}
-          message="Write permission not granted. Please grant the permission in the app settings to export the CSV file."
-          onClose={closeModal}
-          onSettingsPress={openAppSettings}
-        />
       </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  infoExportationView: {
+  exportView: {
     flex: 1,
     padding: 20,
     alignItems: 'center',
@@ -126,40 +94,6 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
   },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  modalMessageText: {
-    fontSize: 18,
-    marginBottom: 20,
-    textAlign: 'center',
-  },
-  modalButton: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginVertical: 5,
-  },
-  okButton: {
-    backgroundColor: '#007BFF',
-  },
-  settingsButton: {
-    backgroundColor: 'gray',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
-  },
 });
 
-export default InfoExportationView;
+export default ExcelExportView;
